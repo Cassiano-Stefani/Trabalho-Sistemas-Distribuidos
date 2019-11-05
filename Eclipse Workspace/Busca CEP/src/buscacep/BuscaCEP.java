49139a48b1ab561d97910bc5d4fce5f81bd283d6
CjMS2103 <cassiano_stefani@hotmail.com>
2019-11-05 15:06:45 -0300

    Progresso na Busca CEP

--- Eclipse Workspace/Busca CEP/src/buscacep/BuscaCEP.java
+++ Eclipse Workspace/Busca CEP/src/buscacep/BuscaCEP.java
@@ -1,19 +1,67 @@
 package buscacep;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class BuscaCEP {
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		while (true) {
-			CEPCompleto cep = CEPUtils.buscarCEP(s.nextLine());
+	private static String ipBancoDeDados = "localhost";
+	private static int portaBancoDeDados = 5111;
 
-			if (cep.erro == null) {
-				System.out.println(cep);
-			} else {
-				System.out.println(cep.erro);
-			}
+	private static int porta = 5110;
+	private static ServerSocket socketServidor;
+
+	private static Socket socketBancoDeDados;
+	private static BufferedReader inBancoDeDados;
+	private static BufferedWriter outBancoDeDados;
+
+	private static Socket socketFrontEnd;
+	private static BufferedReader inFrontEnd;
+	private static BufferedWriter outFrontEnd;
+
+	public static void main(String[] args) {
+		inicializarSockets();
+		inicializarFluxos();
+	}
+
+	public static void inicializarSockets() {
+		try {
+			socketServidor = new ServerSocket(porta);
+		} catch (Exception e) {
+			// TODO
+		}
+
+		try {
+			socketFrontEnd = socketServidor.accept();
+		} catch (Exception e) {
+			// TODO
+		}
+
+		try {
+			socketBancoDeDados = new Socket(InetAddress.getByName(ipBancoDeDados), portaBancoDeDados);
+		} catch (Exception e) {
+			// TODO
+		}
+	}
+
+	public static void inicializarFluxos() {
+		try {
+			inBancoDeDados = new BufferedReader(new InputStreamReader(socketBancoDeDados.getInputStream()));
+			outBancoDeDados = new BufferedWriter(new OutputStreamWriter(socketBancoDeDados.getOutputStream()));
+		} catch (Exception e) {
+			// TODO
+		}
+		
+		try {
+			inFrontEnd = new BufferedReader(new InputStreamReader(socketFrontEnd.getInputStream()));
+			outFrontEnd = new BufferedWriter(new OutputStreamWriter(socketFrontEnd.getOutputStream()));
+		} catch (Exception e) {
+			// TODO
 		}
 	}
 
