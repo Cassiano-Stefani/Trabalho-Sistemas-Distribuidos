dfb7f5663ca60097fc11883cdb781a9b12c8da6a
CjMS2103 <cassiano_stefani@hotmail.com>
2019-11-16 17:38:32 -0300

    Login github

--- Eclipse Workspace/Login Social Tomcat/src/trabalho/UsuariosLogados.java
+++ Eclipse Workspace/Login Social Tomcat/src/trabalho/UsuariosLogados.java
@@ -5,22 +5,31 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import utilidade.Usuario;
+
 public class UsuariosLogados {
 	private static Map<Usuario, HttpSession> logadosGoogle = new HashMap<>();
+	private static Map<Usuario, HttpSession> logadosGithub = new HashMap<>();
 
 	public static void logarUsuario(Usuario usuario, HttpSession sessao) {
 		if (usuario.getApiUsada().equals("google")) {
 			logadosGoogle.put(usuario, sessao);
+		} else if (usuario.getApiUsada().equals("github")) {
+			logadosGithub.put(usuario, sessao);
 		}
 	}
 
 	public static Usuario getUsuarioBySessao(HttpSession sessao) {
-		
+
 		for (Usuario u : logadosGoogle.keySet()) {
-			if(logadosGoogle.get(u) != null && logadosGoogle.get(u).equals(sessao))
+			if (logadosGoogle.get(u) != null && logadosGoogle.get(u).equals(sessao))
 				return u;
 		}
-		// TODO fazer o mesmo para outras apis
+
+		for (Usuario u : logadosGithub.keySet()) {
+			if (logadosGithub.get(u) != null && logadosGithub.get(u).equals(sessao))
+				return u;
+		}
 
 		return null; // usuario nao esta logado
 	}
@@ -28,6 +37,8 @@ public class UsuariosLogados {
 	public static void deslogarUsuario(Usuario usuario) {
 		if (usuario.getApiUsada().equals("google")) {
 			logadosGoogle.put(usuario, null);
+		} else if (usuario.getApiUsada().equals("github")) {
+			logadosGithub.put(usuario, null);
 		}
 	}
 }
